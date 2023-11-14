d66f12049c03fc43a14e077945139de0eeb48af5
sepehr99p <sepehr.pourekrami@gmail.com>
2023-11-14 16:20:05 +0330

    show hourly forecast based on current time

--- app/src/main/java/com/plcoding/weatherapp/presentation/WeatherForecast.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherForecast.kt
@@ -1,5 +1,6 @@
 package com.plcoding.weatherapp.presentation
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -9,6 +10,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.sql.Timestamp
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun WeatherForecast(
@@ -29,12 +34,17 @@ fun WeatherForecast(
             Spacer(modifier = Modifier.height(16.dp))
             LazyRow(content = {
                 items(data) { weatherData ->
-                    HourlyWeatherDisplay(
-                        weatherData = weatherData,
-                        modifier = Modifier
-                            .height(100.dp)
-                            .padding(horizontal = 16.dp)
-                    )
+                    val date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm").parse(weatherData.time.toString())
+                    date?.let {
+                        if (it.time > System.currentTimeMillis()) {
+                            HourlyWeatherDisplay(
+                                weatherData = weatherData,
+                                modifier = Modifier
+                                    .height(100.dp)
+                                    .padding(horizontal = 16.dp)
+                            )
+                        }
+                    }
                 }
             })
         }
